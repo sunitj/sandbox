7f3f99eff03a0ef35456e7493e9f852e0db57f6b
sunitj <sunitj@umich.edu>
2015-07-24 09:54:48 -0400

    added comments; removed unnecessary cleaning step

--- TimeSheet/generateReport.sh
+++ TimeSheet/generateReport.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Generate Report
 Rscript -e "rmarkdown::render('report.Rmd')" &> report.log
 
+# Move report and related files to appropriate location
 MONTHS=(ZERO January February March April May June July August September October November December)
 this_month=`date +%m`
 this_year=`date +%Y`
-mv Figs *.pdf *.log data/${MONTHS[$this_month]}_${this_year}/
+mv *.pdf *.log data/${MONTHS[$this_month]}_${this_year}/
